6e25aebd2da31f9eb5f2b30b300d8e99b5122626
Anubhav <anubhav1691@gmail.com>
2016-05-18 00:22:06 +0000

    initial commit

---
+++ language_detector/.~c9_invoke_ip6bzR.py
@@ -0,0 +1,32 @@
+
+
+"""This is the entry point of the program."""
+
+from .languages import LANGUAGES
+
+
+def detect_language(text, languages):
+    """Returns the detected language of given text."""
+    # implement your solution here
+    set_text = set(text.split())
+    # input text converted to set
+    set_text = set(text.split())
+
+    for language in languages:
+        language['set_common_words'] = set(language['common_words'])
+
+
+    for language in languages:
+        language['score'] = len(set_text.intersection(language['set_common_words']))
+
+    # Longest length is the correct language.
+    sum = 0
+    for language in languages:
+        if language['score'] > sum:
+            sum = language['score']
+    print languages
+    
+    # Return the name of the language with the best score
+    for language in languages:
+        if language['score'] == sum:
+            return language['name']
--- language_detector/main.py
+++ language_detector/main.py
@@ -2,8 +2,53 @@
 
 from .languages import LANGUAGES
 
+def create_set_from_text(text):
+    """
+    return a set of the text
+    """
+    return set(text.split())
 
+def create_set_common_words(languages):
+    """
+    create set of common words
+    """
+    for language in languages:
+        language['set_common_words'] = set(language['common_words'])
+
+def generate_score(languages, set_text):
+    """
+    Add score to each language in languages.
+    """
+    for language in languages:
+        language['score'] = len(set_text.intersection(language['set_common_words']))
+    score = get_highest_score(languages)
+    if language['score'] == score:
+        return language['name']
+            
+def get_highest_score(languages):
+    """
+    Get the highest score from the languages
+    """
+    sum = 0
+    for language in languages:
+        if language['score'] > sum:
+            sum = language['score']
+            return sum     
+  
+def compute_language(languages, score):
+    """
+    Return name of language with the max score
+    """
+    for language in languages:
+        if language['score'] == score:
+            return language['name']
+      
 def detect_language(text, languages):
     """Returns the detected language of given text."""
     # implement your solution here
-    pass
+    set_text = create_set_from_text(text)
+    create_set_common_words(languages)
+    language = generate_score(languages, set_text)
+    score = get_highest_score(languages)
+    language = compute_language(languages, score)
+    return language
